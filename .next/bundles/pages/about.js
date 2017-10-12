ce645deb35c353553e5c0e119321dbc6ced1fee4
Marlon Gumban <msg2msg2@gmail.com>
2017-10-12 22:18:07 +0800

    continued nextjs learning sample batman_app

---
+++ .next/bundles/pages/about.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([6],{110:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(1),a=l(n),r=u(69),o=l(r);t.default=function(){return a.default.createElement("div",null,a.default.createElement(o.default,null,a.default.createElement("p",null,"This is the about page")))}},230:function(e,t,u){e.exports=u(110)}},[230]);
+            return { page: comp.default }
+          })
+        
--- .next/dist/pages/post.js
+++ .next/dist/pages/post.js
@@ -4,13 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _regenerator = require('babel-runtime/regenerator');
+var _style = require('styled-jsx\\style.js');
 
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
-
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+var _style2 = _interopRequireDefault(_style);
 
 var _react = require('react');
 
@@ -20,72 +16,18 @@ var _MyLayout = require('../components/MyLayout.js');
 
 var _MyLayout2 = _interopRequireDefault(_MyLayout);
 
-var _isomorphicUnfetch = require('isomorphic-unfetch');
+var _reactMarkdown = require('react-markdown');
 
-var _isomorphicUnfetch2 = _interopRequireDefault(_isomorphicUnfetch);
+var _reactMarkdown2 = _interopRequireDefault(_reactMarkdown);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = 'C:\\My_Programming_Folder\\NextJS\\batman_app\\pages\\post.js?entry';
-
-
-var Post = function Post(props) {
-  return _react2.default.createElement(_MyLayout2.default, {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 5
-    }
-  }, _react2.default.createElement('h1', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 6
-    }
-  }, props.show.name), _react2.default.createElement('p', {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 7
-    }
-  }, props.show.summary.replace(/<[/]?p>/g, '')), _react2.default.createElement('img', { src: props.show.image.medium, __source: {
-      fileName: _jsxFileName,
-      lineNumber: 8
-    }
+exports.default = function (props) {
+  return _react2.default.createElement(_MyLayout2.default, null, _react2.default.createElement('h1', {
+    'data-jsx': 1238997497
+  }, props.url.query.title), _react2.default.createElement('div', { className: 'markdown', 'data-jsx': 1238997497
+  }, _react2.default.createElement(_reactMarkdown2.default, { source: '\nThis is our blog post.\nYes. We can have a [link](/link).\nAnd we can have a title as well.\n\n### This is a title\n\nAnd here\'s the content.\n     ' })), _react2.default.createElement(_style2.default, {
+    styleId: 1238997497,
+    css: '.markdown{font-family:\'Arial\'}.markdown a{-webkit-text-decoration:none;text-decoration:none;color:blue}.markdown a:hover{opacity:0.6}.markdown h3{margin:0;padding:0;-webkit-text-transform:uppercase;text-transform:uppercase}'
   }));
 };
-
-Post.getInitialProps = function () {
-  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(context) {
-    var id, res, show;
-    return _regenerator2.default.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            id = context.query.id;
-            _context.next = 3;
-            return (0, _isomorphicUnfetch2.default)('https://api.tvmaze.com/shows/' + id);
-
-          case 3:
-            res = _context.sent;
-            _context.next = 6;
-            return res.json();
-
-          case 6:
-            show = _context.sent;
-
-            console.log('Fetched show: ' + show.name);
-
-            return _context.abrupt('return', { show: show });
-
-          case 9:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, _callee, this);
-  }));
-
-  return function (_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
-
-exports.default = Post;
